d2a6134202ab7ae47d4a284a55d3b250b42878da
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-08 09:08:23 +0530

    Update kernel_setup.sh

--- kernel_setup.sh
+++ kernel_setup.sh
@@ -8,7 +8,7 @@
 
 # Config
 CURRENT_DIR="$(pwd)"
-KERNELNAME="Delta"
+KERNELNAME="PhoenixKernel"
 KERNEL_DIR="$CURRENT_DIR"
 AK_REPO="https://github.com/TheStaticDesign/AnyKernel3"
 AK_BRANCH="sweet"
